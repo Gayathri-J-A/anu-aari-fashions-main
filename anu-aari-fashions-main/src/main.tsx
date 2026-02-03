ed7c26b963be97c18d0116eca1adbbba4d1a047b
Gayathri J.A <jayayathri722@gmail.com>
2026-02-03 17:19:53 +0530

    Update repository with local changes

--- anu-aari-fashions-main/src/main.tsx
+++ anu-aari-fashions-main/src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+<<<<<<< HEAD
 import { HashRouter } from "react-router-dom";
 import App from './App';
+=======
+import { HashRouter } from "react-router-dom"; // changed
+import App from "./App.tsx";
+>>>>>>> ebbb470 (Commit all changes including deletions and new files)
 import "./index.css";
 const rootElement = document.getElementById("root");
 const routerBasename = import.meta.env.PROD ? "/anu-aari-fashions-main" : "/";
@@ -12,9 +17,17 @@ const routerBasename = import.meta.env.PROD ? "/anu-aari-fashions-main" : "/";
   const root = createRoot(rootElement); 
   root.render(
   <StrictMode>
+<<<<<<< HEAD
     <HashRouter basename={routerBasename}>
     <App />
    </HashRouter>
   </StrictMode>
 );
   }
+=======
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
+>>>>>>> ebbb470 (Commit all changes including deletions and new files)
